1dc58d6798b6237548637e0c94e4e8ccdb5176ec
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2011-11-03 07:24:29 +0100

    Again working tests

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -13,6 +13,6 @@ class DemoControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/demo/hello/Fabien');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-        $this->assertTrue(false);
+        //$this->assertTrue(false);
     }
 }
