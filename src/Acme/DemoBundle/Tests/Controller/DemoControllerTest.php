fe4b83af56f83a076bc095d4b3e68813f7cb6d69
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2011-11-03 07:22:07 +0100

    Test a failing unittest

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -13,5 +13,6 @@ class DemoControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/demo/hello/Fabien');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue(false);
     }
 }
