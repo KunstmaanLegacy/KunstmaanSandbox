b296e11fdf5989861117814ad5f3afc7c40612ea
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2011-11-05 09:50:33 +0100

    Add the Airbrake bundle to track exceptions.
    Current API key in parameters.ini is for the OpenMercury.NEXT project
    in Airbrake. Request a new one if you start a new project.

--- app/autoload.php
+++ app/autoload.php
@@ -15,6 +15,7 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'Elao'             => __DIR__.'/../vendor/bundles',
+    'Airbrake'         => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
