848fb5e32e405aa1fa62e8e76188b4cd05a4f1c6
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2011-11-08 14:18:51 +0100

    login for backend

---
+++ src/Kunstmaan/KAdminBundle/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+// src/KAdminBundle/Entity/User.php
+
+namespace Blogger\BlogBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class KUser extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
