7ab9243b68370c6506c018160585715704578fac
Kris Pypen <kris.pypen@kunstmaan.be>
2011-11-17 17:49:07 +0100

    moved nodes to specific bundle

---
+++ src/Kunstmaan/KAdminNodeBundle/Entity/HasNode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kris
+ * Date: 14/11/11
+ * Time: 16:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Kunstmaan\KAdminNodeBundle\Entity;
+
+interface HasNode {
+
+    public function getId();
+
+}
---
+++ src/Kunstmaan/KAdminNodeBundle/KunstmaanKAdminNodeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kunstmaan\KAdminNodeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KunstmaanKAdminNodeBundle extends Bundle
+{
+}
