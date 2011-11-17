3581c28560cc08ddf5407a6a44b46f705891f161
Kris Pypen <kris.pypen@kunstmaan.be>
2011-11-17 11:56:27 +0100

    adminlist

--- src/Kunstmaan/KAdminBundle/Controller/SettingsController.php
+++ src/Kunstmaan/KAdminBundle/Controller/SettingsController.php
@@ -3,7 +3,7 @@
 namespace Kunstmaan\KAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Kunstmaan\KAdminBundle\AdminList\UserAdminListConfigurator;
 
 class SettingsController extends Controller
 {
@@ -11,6 +11,17 @@ class SettingsController extends Controller
     public function indexAction()
     {
         return $this->render('KunstmaanKAdminBundle:Settings:index.html.twig');
+    }
+
+    public function usersAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $request = $this->getRequest();
+        $adminlist    = $this->get("adminlist.factory")->createList(new UserAdminListConfigurator(), $em);
+        $adminlist->bindRequest($request);
+        return $this->render('KunstmaanKAdminBundle:Settings:users.html.twig', array(
+            'useradminlist'    => $adminlist,
+        ));
     }
 	
 }
