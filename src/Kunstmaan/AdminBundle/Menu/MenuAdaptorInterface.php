daf573d0cb6913afe7e8439cfaea41e1aeb89047
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2011-12-19 10:49:25 +0100

    menu translations

--- src/Kunstmaan/AdminBundle/Menu/MenuAdaptorInterface.php
+++ src/Kunstmaan/AdminBundle/Menu/MenuAdaptorInterface.php
@@ -2,11 +2,13 @@
 // src/Acme/DemoBundle/Menu/Builder.php
 namespace Kunstmaan\AdminBundle\Menu;
 
+use Symfony\Component\Translation\Translator;
+
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Knp\Menu\ItemInterface as KnpMenu;
 
 interface MenuAdaptorInterface
 {
-    function adaptMenu(KnpMenu $menu);
+    function adaptMenu(KnpMenu $menu, Translator $translator);
 }
