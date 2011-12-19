c85564c3dbc1c75e6d2bf9e5844f3d7673249a8f
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2011-12-19 12:41:37 +0100

    locale switcher

--- src/Kunstmaan/AdminBundle/Controller/DefaultController.php
+++ src/Kunstmaan/AdminBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace Kunstmaan\AdminBundle\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
@@ -13,4 +15,17 @@ class DefaultController extends Controller
         return $this->render('KunstmaanAdminBundle:Default:index.html.twig');
     }
 	
+    /**
+     * @param string $locale
+     * @return array
+     */
+    public function setLocaleAction($locale) {
+    	/** @var \Symfony\Component\HttpFoundation\Session $session  */
+    	$session = $this->get('session');
+    	$session->setLocale($locale);
+    	$last_route = $session->get('last_route', array('name' => 'index'));
+    	$last_route['params']['_locale'] = $locale;
+    	return ($this->redirect($this->generateUrl($last_route['name'], $last_route['params'])));
+    }
+    
 }
