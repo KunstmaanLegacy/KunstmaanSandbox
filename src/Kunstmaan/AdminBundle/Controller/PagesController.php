1d900106f1f12ea8a1eb601cb889b9575b3f9ec0
Kris Pypen <kris.pypen@kunstmaan.be>
2012-01-12 12:43:28 +0100

    working draft/public pageparts

--- src/Kunstmaan/AdminBundle/Controller/PagesController.php
+++ src/Kunstmaan/AdminBundle/Controller/PagesController.php
@@ -46,7 +46,7 @@ class PagesController extends Controller
     }
 
 /**
-     * @Route("/admin/pages/{id}/publish", requirements={"_method" = "GET|POST", "id" = "\d+"}, name="KunstmaanAdminBundle_pages_edit_publish")
+     * @Route("/{id}/publish", requirements={"_method" = "GET|POST", "id" = "\d+"}, name="KunstmaanAdminBundle_pages_edit_publish")
      * @Template()
      */
     public function publishAction($id)
@@ -60,7 +60,7 @@ class PagesController extends Controller
     }
     
     /**
-     * @Route("/admin/pages/{id}/unpublish", requirements={"_method" = "GET|POST", "id" = "\d+"}, name="KunstmaanAdminBundle_pages_edit_unpublish")
+     * @Route("/{id}/unpublish", requirements={"_method" = "GET|POST", "id" = "\d+"}, name="KunstmaanAdminBundle_pages_edit_unpublish")
      * @Template()
      */
     public function unpublishAction($id)
@@ -74,7 +74,7 @@ class PagesController extends Controller
     }
     
     /**
-     * @Route("/admin/pages/{id}/{subaction}", requirements={"_method" = "GET|POST", "id" = "\d+"}, defaults={"subaction" = "public"}, name="KunstmaanAdminBundle_pages_edit")
+     * @Route("/{id}/{subaction}", requirements={"_method" = "GET|POST", "id" = "\d+"}, defaults={"subaction" = "public"}, name="KunstmaanAdminBundle_pages_edit")
      * @Template()
      */
     public function editAction($id, $subaction)
@@ -125,7 +125,7 @@ class PagesController extends Controller
         	return $this->redirect($this->generateUrl("KunstmaanAdminBundle_pages_edit", array('id'=>$nodeparent->getId())));
         }
 
-		$page = $em->getRepository(ClassLookup::getClass($page))->find($id);  //'KunstmaanAdminBundle:Page'
+		//$page = $em->getRepository(ClassLookup::getClass($page))->find($id);  //'KunstmaanAdminBundle:Page'
         $page->setTranslatableLocale($locale);
         $em->refresh($page);
         $repo = $em->getRepository('StofDoctrineExtensionsBundle:LogEntry');
@@ -133,10 +133,9 @@ class PagesController extends Controller
         if(!is_null($this->getRequest()->get('version'))) {
         	$repo->revert($page, $this->getRequest()->get('version'));
         }
-
         $user = $this->container->get('security.context')->getToken()->getUser();
         $topnodes   = $em->getRepository('KunstmaanAdminNodeBundle:Node')->getTopNodes($user, 'write');
-        $node       = $em->getRepository('KunstmaanAdminNodeBundle:Node')->getNodeFor($page);
+        //$node       = $em->getRepository('KunstmaanAdminNodeBundle:Node')->getNodeFor($page);
 
         $formfactory = $this->container->get('form.factory');
         $formbuilder = $this->createFormBuilder();
@@ -148,6 +147,7 @@ class PagesController extends Controller
         $formbuilder->setData(array('node' => $node, 'main' => $page));
 
         //handle the pagepart functions (fetching, change form to reflect all fields, assigning data, etc...)
+
         $pagepartadmin = $this->get("pagepartadmin.factory")->createList(new PagePartAdminConfigurator(), $em, $page);
         $pagepartadmin->preBindRequest($request);
         $pagepartadmin->adaptForm($formbuilder, $formfactory);
