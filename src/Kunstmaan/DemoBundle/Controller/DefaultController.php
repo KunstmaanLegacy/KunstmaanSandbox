6cd600b82ed192ae4f63aa2f813f8a1c24f1a35a
Kris Pypen <kris.pypen@kunstmaan.be>
2012-02-24 15:17:02 +0100

    travis config

--- src/Kunstmaan/DemoBundle/Controller/DefaultController.php
+++ src/Kunstmaan/DemoBundle/Controller/DefaultController.php
@@ -2,13 +2,20 @@
 
 namespace Kunstmaan\DemoBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class DefaultController extends Controller
 {
 
-    public function indexAction($name)
+	/**
+	 * @Route("/", name="KunstmaanAdminBundle_homepage")
+	 * @Template()
+	 */
+    public function indexAction()
     {
         return $this->forward("KunstmaanViewBundle:Slug:slug", array("_locale"=>"en", "slug"=>"home"));
     }
