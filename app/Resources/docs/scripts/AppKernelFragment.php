e37a2aa6a0b2b52d1b607c8b4e5231bf3c85e03d
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-08-30 10:17:33 +0200

    Add scripts from gists

---
+++ app/Resources/docs/scripts/AppKernelFragment.php
@@ -0,0 +1,38 @@
+        // KunstmaanAdminBundle
+        $bundles[] = new FOS\UserBundle\FOSUserBundle();
+        $bundles[] = new Knp\Bundle\MenuBundle\KnpMenuBundle();
+        $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        $bundles[] = new Kunstmaan\AdminBundle\KunstmaanAdminBundle();
+        // KunstmaanMediaBundle
+        $bundles[] = new Liip\ImagineBundle\LiipImagineBundle();
+        $bundles[] = new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle();
+        $bundles[] = new Kunstmaan\MediaBundle\KunstmaanMediaBundle();
+        // KunstmaanPagePartBundle
+        $bundles[] = new Kunstmaan\PagePartBundle\KunstmaanPagePartBundle();
+        // KunstmaanMediaPagePartBundle
+        $bundles[] = new Kunstmaan\MediaPagePartBundle\KunstmaanMediaPagePartBundle();
+        // KunstmaanFormBundle
+        $bundles[] = new Kunstmaan\FormBundle\KunstmaanFormBundle();
+        // KunstmaanAdminListBundle
+        $bundles[] = new Kunstmaan\AdminListBundle\KunstmaanAdminListBundle();
+        // KunstmaanAdminNodeBundle
+        $bundles[] = new Kunstmaan\AdminNodeBundle\KunstmaanAdminNodeBundle();
+        // KunstmaanViewBundle
+        $bundles[] = new Kunstmaan\ViewBundle\KunstmaanViewBundle();
+        // KunstmaanSearchBundle
+        $bundles[] = new FOQ\ElasticaBundle\FOQElasticaBundle();
+        $bundles[] = new Kunstmaan\SearchBundle\KunstmaanSearchBundle();
+        // KunstmaanGeneratorBundle
+        $bundles[] = new Kunstmaan\GeneratorBundle\KunstmaanGeneratorBundle();
+        // LiipMonitorBundle & LiipMonitorExtraBundle
+        $bundles[] = new Liip\MonitorBundle\LiipMonitorBundle();
+        $bundles[] = new Liip\MonitorExtraBundle\LiipMonitorExtraBundle();
+        // LiipCacheControlBundle
+        $bundles[] = new Liip\CacheControlBundle\LiipCacheControlBundle();
+
+        if (in_array($this->getEnvironment(), array('prod'))){
+            // KunstmaanSentryBundle
+            $bundles[] = new Kunstmaan\SentryBundle\KunstmaanSentryBundle();
+        }
+
+        return $bundles;
---
+++ app/Resources/docs/scripts/sandboxinstaller.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/ruby
+
+require 'json'
+require 'open-uri'
+require 'yaml'
+
+command = ARGV[0]
+
+if command == "install-bundles"
+    puts "Let's install the bundles"
+    composer = ARGV[1]
+    appkernel = ARGV[2]
+
+    # Composer.json
+    buffer = open(composer).read
+    result = JSON.parse(buffer)
+    requires = result['require']
+    requires["kunstmaan/admin-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/media-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/pagepart-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/media-pagepart-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/form-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/adminlist-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/adminnode-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/view-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/search-bundle"] =  "2.0.x-dev"
+    requires["kunstmaan/generator-bundle"] = "2.0.x-dev"
+    requires["kunstmaan/sentry-bundle"] = "dev-master"
+    requires["liip/monitor-bundle"] = "dev-master"
+    requires["liip/monitor-extra-bundle"] = "dev-master"
+    requires["liip/cache-control-bundle"] = "dev-master"
+    File.open(composer, 'w') {|f| f.write(JSON.pretty_generate(result)) }
+
+    # AppKernel.php
+    appk = File.read(appkernel)
+    if not appk.include? "Kunstmaan"
+        buffer = open('https://raw.github.com/gist/3424023/AppKernelFragment.php').read
+        replaced = appk.gsub("return $bundles;", buffer)
+        File.open(appkernel, 'w') {|f| f.write(replaced) }
+    else
+        puts "AppKernel.php already contains Kunstmaan bundle configurations."
+    end
+elsif command == "configure-bundles"
+    parametersymlpath = ARGV[1]
+    projectname = ARGV[2]
+    parametersyml = YAML.load_file(parametersymlpath)
+    params = parametersyml["parameters"]
+    params["searchport"] = 9200
+    params["searchindexname"] = projectname
+    params["sentry.dsn"] = "https://XXXXXXXX:XXXXXXXX@app.getsentry.com/XXXX"
+    params["cdnpath"] = ""
+    params["requiredlocales"] = "nl|fr|de|en"
+    params["defaultlocale"] = "nl"
+    params["websitetitle"] = projectname.capitalize
+    File.open(parametersymlpath, 'w') {|f| f.write(YAML.dump(parametersyml)) }
+else
+    puts "No command by this name found."
+    puts ""
+    puts "Possible commands are:"
+    puts "    install-bundles [path to composer.json] [path to AppKernel.php]"
+    puts "    configure-bundles [path to parameters.yml] [projectname]"
+end
